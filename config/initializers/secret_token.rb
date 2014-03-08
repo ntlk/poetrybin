0f97c6505d27447ed3af63a749f1e513dada328c
ntlk <natalia.buckley@gmail.com>
2014-03-08 16:39:27 +0000

    Generate a new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poetrybin::Application.config.secret_key_base = '88b74374b2c5001416676308ddf94aa0fb99e91dfecbab6d50b30a49fc9c00a3b4248be9107a938594c7a64b6b6bf15f884e6c80b17faab06d58e258af86c53a'
