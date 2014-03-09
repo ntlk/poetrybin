33d9b30c990c0ba655d73190fa859e9db6d033c5
ntlk <hi@ntlk.net>
2014-03-09 16:46:46 +0000

    Finish discovery of adding poems

--- features/step_definitions/exploration_steps.rb
+++ features/step_definitions/exploration_steps.rb
@@ -14,3 +14,15 @@ Then(/^I see a random poem in full$/) do
   expect_page_to_have_poem(Poem.first)
 end
 
+When(/^I click on new poem link$/) do
+  click_on('Add a poem')
+end
+
+Then(/^I can create a new poem$/) do
+  fill_in('Title', with: 'A cat')
+  fill_in('Poem', with: 'This is my little poem')
+  click_on('Save')
+  visit poems_path
+  expect(page).to have_link('A cat')
+end
+
