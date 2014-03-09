83313abd4429892631e31448a3176c03c7a47c52
ntlk <natalia.buckley@gmail.com>
2014-03-09 20:41:20 +0000

    Add newest_first scope

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,6 +1,7 @@
 class Poem < ActiveRecord::Base
   validates :body, uniqueness: true, presence: true
-  scope :visible, -> { where(hidden: false) }
+  scope :visible, -> {where(hidden: false)}
+  scope :newest_first, -> {order('created_at DESC')}
 
   def self.random
     order('RANDOM()').first
