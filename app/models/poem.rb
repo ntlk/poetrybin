85955063701a05e92551ff4eb0cdddaaeb0ca00c
ntlk <hi@ntlk.net>
2014-03-09 20:39:19 +0000

    Make #random return an object

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,5 +1,8 @@
 class Poem < ActiveRecord::Base
   validates :body, uniqueness: true, presence: true
-  scope :random, -> { order('RANDOM()') }
   scope :visible, -> { where(hidden: false) }
+
+  def self.random
+    order('RANDOM()').first
+  end
 end
