30d951b9c64c4e9fbf83dc1fa13c9e1d3aa9b124
ntlk <hi@ntlk.net>
2014-03-09 17:46:13 +0000

    Rename AuthenticationController to SessionsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Poetrybin::Application.routes.draw do
   resources :poems
   get 'random', to: 'poems#random'
-  get 'admin_access', to: 'authentication#login'
-  post 'admin_access', to: 'authentication#check_password'
-  put 'logout', to: 'authentication#logout'
+  get 'admin_access', to: 'sessions#login'
+  post 'admin_access', to: 'sessions#check_password'
+  put 'logout', to: 'sessions#logout'
   root to: 'poems#random'
 end
