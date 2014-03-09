818cf8caee5734259fe8c46777a97edfeb3ca279
ntlk <natalia.buckley@gmail.com>
2014-03-09 16:46:19 +0000

    Make annoying message go away

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Poetrybin
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
+    I18n.enforce_available_locales = false
   end
 end
