c4ce54e14f3bb88ca44cf61de9f2d829729b767f
ntlk <natalia.buckley@gmail.com>
2014-03-11 22:26:17 +0000

    Update writing feature to include html escaping and basic formatting

--- features/step_definitions/writing_steps.rb
+++ features/step_definitions/writing_steps.rb
@@ -4,7 +4,7 @@ end
 
 When(/^I type in a poem$/) do
   fill_in('Title', with: 'A title')
-  fill_in('Poem', with: 'Some poem body\nWith basic formatting')
+  fill_in('Poem', with: 'Some poem body with no formatting')
   click_on('Save')
 end
 
@@ -12,3 +12,26 @@ Then(/^it appears on the site$/) do
   expect_page_to_have_poem(Poem.find_by_title('A title'))
 end
 
+When(/^I type in a poem with formatting$/) do
+  fill_in('Title', with: 'A title')
+  fill_in('Poem', with: 'Some poem body\nwith some formatting\n**bold** text\n*italics*\n# and font\n## sizes')
+  click_on('Save')
+end
+
+Then(/^it appears on the site with formatting$/) do
+  expect(page).to have_content('Some poem body<br>with some formatting<br>')
+  expect(page).to have_content('<strong>bold</strong> text')
+  expect(page).to have_content('<em>italics</em>')
+  expect(page).to have_content('<span class="size-one">and font</span>')
+  expect(page).to have_content('<span class="size-two">sizes</span>')
+end
+
+When(/^I type in a poem with html$/) do
+  fill_in('Title', with: 'A title')
+  fill_in('Poem', with: 'Some poem body <strong>with html</strong>')
+  click_on('Save')
+end
+
+Then(/^it appears on the site with html tags escaped$/) do
+  expect(page).to have_content('Some poem body &lt;strong&gt;with html&lt;/strong&gt;')
+end
--- features/writing.feature
+++ features/writing.feature
@@ -8,6 +8,16 @@ Feature: writing poems
     When I type in a poem
     Then it appears on the site
 
+  Scenario: create a new poem with some formatting
+    Given that I'm on the new poem page
+    When I type in a poem with formatting
+    Then it appears on the site with formatting
+
+  Scenario: create a new poem which includes html tags
+    Given that I'm on the new poem page
+    When I type in a poem with html
+    Then it appears on the site with html tags escaped
+
   Scenario: edit existing poem
     Given that I have created a poem in the last 15 minutes
     When I go to edit poem page
