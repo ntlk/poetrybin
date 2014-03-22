ddc6542d6d1d801e59c4ad077f0588641ced4a88
ntlk <natalia.buckley@gmail.com>
2014-03-22 20:54:07 +0000

    Test medium size

--- spec/lib/formatter_spec.rb
+++ spec/lib/formatter_spec.rb
@@ -161,7 +161,7 @@ describe Formatter do
         expect(Formatter.change_text_sizes(string)).to eq string
       end
 
-      it 'resizes the line when # is present' do
+      it 'resizes the line when ## is present' do
         string = '##hello'
         expect(Formatter.change_text_sizes(string)).to eq '<span class="size-two">hello</span>'
       end
@@ -174,6 +174,28 @@ describe Formatter do
       it 'resizes the ## line when multiple lines are present' do
         string = "hello\n##hello\nhi there"
         expect(Formatter.change_text_sizes(string)).to eq "hello\n<span class=\"size-two\">hello</span>\nhi there"
+      end
+    end
+
+    context 'medium size' do
+      it 'returns same string if hash is not at the beginning of line' do
+        string = 'hi, ### hello'
+        expect(Formatter.change_text_sizes(string)).to eq string
+      end
+
+      it 'resizes the line when ### is present' do
+        string = '###hello'
+        expect(Formatter.change_text_sizes(string)).to eq '<span class="size-three">hello</span>'
+      end
+
+      it 'resizes the line when ###\s is present' do
+        string = '### hello'
+        expect(Formatter.change_text_sizes(string)).to eq '<span class="size-three">hello</span>'
+      end
+
+      it 'resizes the ### line when multiple lines are present' do
+        string = "hello\n###hello\nhi there"
+        expect(Formatter.change_text_sizes(string)).to eq "hello\n<span class=\"size-three\">hello</span>\nhi there"
       end
     end
   end
