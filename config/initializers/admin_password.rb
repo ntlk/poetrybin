c6e04b1f010ec255d9e0b18dd38b39bf55346904
Tom Stuart <tom@codon.com>
2014-03-22 23:20:58 +0000

    Set a default admin password in test environment

--- config/initializers/admin_password.rb
+++ config/initializers/admin_password.rb
@@ -1,1 +1,7 @@
-ADMIN_PASSWORD = ENV['PASSWORD'] || raise('Please specify the Poetrybin admin password by setting the PASSWORD environment variable')
+if ENV['PASSWORD'].present?
+  ADMIN_PASSWORD = ENV['PASSWORD']
+elsif Rails.env.test?
+  ADMIN_PASSWORD = 'password'
+else
+  raise('Please specify the Poetrybin admin password by setting the PASSWORD environment variable')
+end
