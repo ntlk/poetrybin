09b2a4a40c5496d2942f422ecf7486feb6751357
ntlk <hi@ntlk.net>
2014-03-23 12:21:14 +0000

    Add and test Poem#display_title

--- spec/models/poems_spec.rb
+++ spec/models/poems_spec.rb
@@ -3,4 +3,31 @@ require 'spec_helper'
 describe Poem do
   it { should validate_presence_of(:body) }
   it { should validate_uniqueness_of(:body) }
+
+  context '#display_title' do
+
+    context 'title present' do
+      it 'should return title' do
+        poem = Poem.create(title: 'poem title', body: 'poem body')
+        expect(poem.display_title).to eq poem.title
+      end
+    end
+
+    context 'without title' do
+      it 'should return the first line of body as display title' do
+        poem = Poem.create(title: nil, body: "an example poem body\nwith a new line")
+        expect(poem.display_title).to eq 'an example poem body'
+      end
+
+      it 'should return the first line of body as display title if title is an empty string' do
+        poem = Poem.create(title: '', body: "an example poem body\nwith a new line")
+        expect(poem.display_title).to eq 'an example poem body'
+      end
+
+      it 'should truncate the body if the line is longer than five words' do
+        poem = Poem.create(title: nil, body: "an example poem which has many words in the first line\nand fewer on second")
+        expect(poem.display_title).to eq 'an example poem which has…'
+      end
+    end
+  end
 end
